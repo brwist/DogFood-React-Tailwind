67f264c99aab84fcc780b72ad6ff46e21c4d0586
amaan-nadeem <amaannadeem@outlook.com>
2021-03-07 19:27:06 +0500

    fix: remove console error of pages/profiles

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,19 +1,19 @@
 /* eslint-disable semi */
-import React from "react";
-import { connect } from "react-redux";
-import { ReactComponent as Arrow } from "../../assets/images/Vectorarrow.svg";
-import LoadingCircle from "../../components/partials/loading.jsx";
-import { userActions } from "../../actions";
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { userActions } from '../../actions';
+import { AccountDetails } from '../../components/profile/account-details';
+import Billing from '../../components/profile/billing';
+import Cupon from '../../components/profile/cupon';
+import { DeliveryAddress } from '../../components/profile/delivery-address';
+import Loader from '../../loaders/profileLoader';
 
-import Billing from "../../components/profile/billing.jsx";
-import { AccountDetails } from "../../components/profile/account-details.jsx";
-import { DeliveryAddress } from "../../components/profile/delivery-address.jsx";
-import Loader from "../../loaders/profileLoader";
-import Cupon from "../../components/profile/cupon";
 class ProfilePage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      /* eslint-disable react/no-unused-state */
       nextExpanded: false,
       mealExpanded: false,
       frequencyExpanded: false,
@@ -21,32 +21,36 @@ class ProfilePage extends React.Component {
     this.openModal = this.openModal.bind(this);
   }
 
+  componentDidMount() {
+    const { getAccountData, getSubscriptionData, getBreedData } = this.props;
+    getAccountData();
+    getSubscriptionData();
+    getBreedData();
+  }
+
   openModal(name) {
+    const { [name]: actualField } = this.state;
     this.setState({
-      [name]: !this.state[name],
+      [name]: !actualField,
     });
   }
 
-  componentDidMount() {
-    this.props.getAccountData();
-    this.props.getSubscriptionData();
-    this.props.getBreedData();
-  }
-
   render() {
-    if (!this.props.dogs.length || !this.props.user.shipping_address)
-      return <Loader />;
     const {
       user,
-      subscriptions,
       dogs,
       updatePaymentMethod,
       addCoupon,
       couponResponse,
       userError,
+      setBillingAddress,
+      showManageSubscriptionsBox,
+      openSubscriptionManagementModal,
+      openUpdatePaymentModal,
     } = this.props;
+    if (!dogs.length || !user.shipping_address) return <Loader />;
 
-    const detailsCard = "container pb-4  shadow-profileBoxes p-10 rounded-xl";
+    const detailsCard = 'container pb-4  shadow-profileBoxes p-10 rounded-xl';
     return (
       <div className="grid lg:grid-cols-2 md:grid-cols-1 gap-7 pb-10">
         <div className={detailsCard}>
@@ -56,23 +60,18 @@ class ProfilePage extends React.Component {
           <Billing
             user={user}
             open_payment_modal={user.open_payment_modal}
-            openUpdatePaymentModal={this.props.openUpdatePaymentModal}
+            openUpdatePaymentModal={openUpdatePaymentModal}
             payment_billing_address={user.payment_billing_address}
-            setBillingAddress={this.props.setBillingAddress}
+            setBillingAddress={setBillingAddress}
             payment_method_updated={user.payment_method_updated}
             updatePaymentMethod={updatePaymentMethod}
             updating_payment_method={user.updating_payment_method}
-            showManageSubscriptionsBox={this.props.showManageSubscriptionsBox}
-            openSubscriptionManagementModal={
-              this.props.openSubscriptionManagementModal
-            }
+            showManageSubscriptionsBox={showManageSubscriptionsBox}
+            openSubscriptionManagementModal={openSubscriptionManagementModal}
           />
         </div>
         <div className={detailsCard}>
-          <DeliveryAddress
-            user={user}
-            deliveryAddress={user.shipping_address}
-          />
+          <DeliveryAddress user={user} deliveryAddress={user.shipping_address} />
         </div>
         <div className={detailsCard}>
           <Cupon
@@ -92,15 +91,14 @@ const mapDispatchToProps = (dispatch) => ({
   getAccountData: () => dispatch(userActions.getAccountData()),
   getSubscriptionData: () => dispatch(userActions.getSubscriptionData()),
   getBreedData: () => dispatch(userActions.getBreedData()),
-  openUpdatePaymentModal: (payload) =>
-    dispatch(userActions.openUpdatePaymentModal(payload)),
-  setBillingAddress: (payload) =>
-    dispatch(userActions.setBillingAddress(payload)),
-  updatePaymentMethod: (payload) =>
-    dispatch(userActions.updatePaymentMethod(payload)),
+  openUpdatePaymentModal: (payload) => dispatch(userActions.openUpdatePaymentModal(payload)),
+  setBillingAddress: (payload) => dispatch(userActions.setBillingAddress(payload)),
+  updatePaymentMethod: (payload) => dispatch(userActions.updatePaymentMethod(payload)),
   addCoupon: (payload) => dispatch(userActions.applyCoupon(payload)),
-  openSubscriptionManagementModal: (payload) =>
-    dispatch(userActions.openSubscriptionManagementModal(payload)),
+  openSubscriptionManagementModal: (payload) => {
+    const { openSubscriptionManagementModal } = userActions;
+    dispatch(openSubscriptionManagementModal(payload));
+  },
 });
 
 const mapStateToProps = (state) => {
@@ -120,6 +118,32 @@ const mapStateToProps = (state) => {
     userError: errorMessage,
     showManageSubscriptionsBox,
   };
+};
+ProfilePage.defaultProps = {
+  showManageSubscriptionsBox: false,
+  updating_payment_method: '',
+  dogs: '',
+  updatePaymentMethod: '',
+  addCoupon: '',
+  couponResponse: '',
+  userError: '',
+};
+
+ProfilePage.propTypes = {
+  openUpdatePaymentModal: PropTypes.func.isRequired,
+  openSubscriptionManagementModal: PropTypes.func.isRequired,
+  showManageSubscriptionsBox: PropTypes.bool,
+  updating_payment_method: PropTypes.string,
+  setBillingAddress: PropTypes.func.isRequired,
+  dogs: PropTypes.shape([]),
+  updatePaymentMethod: PropTypes.string,
+  addCoupon: PropTypes.string,
+  couponResponse: PropTypes.string,
+  userError: PropTypes.string,
+  user: PropTypes.objectOf(PropTypes.any).isRequired,
+  getAccountData: PropTypes.func.isRequired,
+  getSubscriptionData: PropTypes.func.isRequired,
+  getBreedData: PropTypes.func.isRequired,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ProfilePage);
